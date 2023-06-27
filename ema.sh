fa24f4d695ceb1def497f123ca5459a2b04fa117
Diyoro <diyoroothi@gmail.com>
2023-06-27 15:21:28 +0000

    First EMATEC

--- ema.sh
+++ ema.sh
@@ -1,5 +1,5 @@
 git init 
 git add .
 git commit -m "First EMATEC"
-git remote add origin https://github.com/Diyoro/prince007.git
+git remote add origin https://github.com/Diyoro/ematec.git
 git push -u origin master 
