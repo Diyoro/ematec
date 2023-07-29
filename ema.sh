31585651c3277efbc2b5008c1b9af24eeb365f1e
Diyoro <diyoroothi@gmail.com>
2023-07-29 13:33:00 +0000

    Première mise à jour EMATEC

--- ema.sh
+++ ema.sh
@@ -1,5 +1,5 @@
 git init 
 git add .
-git commit -m "First EMATEC"
+git commit -m "Première mise à jour EMATEC"
 git remote add origin https://github.com/Diyoro/ematec.git
 git push -u origin master 
